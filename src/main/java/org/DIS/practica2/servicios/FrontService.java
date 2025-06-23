b463bdbbe9d604225537f78642614e68f99f5cfb
ovifernandez <davidenriquea@icloud.com>
2025-06-23 21:39:50 +0200

    Front service acabado

--- src/main/java/org/DIS/practica2/servicios/FrontService.java
+++ src/main/java/org/DIS/practica2/servicios/FrontService.java
@@ -10,9 +10,11 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 @Service
