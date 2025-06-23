fbb8eb4016796556ad1176c395923bfd9637aed6
burru <burru@LAPTOP-6S6ME6G9>
2025-06-23 21:55:53 +0200

    Estructura basica de las vistas

---
+++ src/main/java/org/DIS/practica2/MainView.java
@@ -0,0 +1,9 @@
+package org.DIS.practica2;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+}
