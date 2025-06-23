03c09611268c1f5867cb517e7496d65031d03191
Borja2004 <9205627@alumnos.ufv.es>
2025-06-23 22:33:36 +0200

    Constructor y variables de la pestaña nuevoturismo

--- src/main/java/org/DIS/practica2/vistas/NuevoTurismo.java
+++ src/main/java/org/DIS/practica2/vistas/NuevoTurismo.java
@@ -1,5 +1,25 @@
 package org.DIS.practica2.vistas;
 
-public class NuevoTurismo {
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.TextField;
+import org.DIS.practica2.servicios.FrontService;
 
+public class NuevoTurismo {
+    TextField comunidadOrigen = new TextField("Comunidad Origen: ");
+    TextField provinciaOrigen = new TextField("Provincia Origen: ");
+    TextField comunidadDestino = new TextField("Comunidad Destino: ");
+    TextField provinciaDestino = new TextField("Provincia Destino: ");
+    DatePicker fechaInicio = new DatePicker("Fecha de Inicio: ");
+    DatePicker fechaFin = new DatePicker("Fecha de Fin: ");
+    IntegerField total = new IntegerField("Total: ");
+    ConfirmDialog dialogoConfirmacion = new ConfirmDialog();
+    FrontService service;
+
+    public NuevoTurismo(FrontService service) {
+        this.service = service;
+
+        configurarDatePickers();
+    }
 }
