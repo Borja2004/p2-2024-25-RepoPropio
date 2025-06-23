c2809aecb165e8af5f0450d63b03890fbe8a173a
Borja2004 <9205627@alumnos.ufv.es>
2025-06-23 13:34:45 +0200

    Clase origen creada

--- src/main/java/org/DIS/practica2/modelos/Origen.java
+++ src/main/java/org/DIS/practica2/modelos/Origen.java
@@ -1,4 +1,35 @@
 package org.DIS.practica2.modelos;
 
 public class Origen {
+    private String provincia;
+    private String comunidad;
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getComunidad() {
+        return comunidad;
+    }
+
+    public void setComunidad(String comunidad) {
+        this.comunidad = comunidad;
+    }
+
+    public Origen(String comunidad, String provincia) {
+        this.comunidad = comunidad;
+        this.provincia = provincia;
+    }
+
+    @Override
+    public String toString() {
+        return "Origen{" +
+                "provincia='" + provincia + '\'' +
+                ", comunidad='" + comunidad + '\'' +
+                '}';
+    }
 }
