32400ab2c4038f4e1e32fa8bcdbb3d2b4a5a3f56
Borja2004 <9205627@alumnos.ufv.es>
2025-06-23 13:33:26 +0200

    Clase destino creada con sus constructores

--- src/main/java/org/DIS/practica2/modelos/Destino.java
+++ src/main/java/org/DIS/practica2/modelos/Destino.java
@@ -1,4 +1,35 @@
 package org.DIS.practica2.modelos;
 
 public class Destino {
+    private String comunidad;
+    private String provincia;
+
+    public String getComunidad() {
+        return comunidad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setComunidad(String comunidad) {
+        this.comunidad = comunidad;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public Destino(String comunidad, String provincia) {
+        this.comunidad = comunidad;
+        this.provincia = provincia;
+    }
+
+    @Override
+    public String toString() {
+        return "Destino{" +
+                "comunidad='" + comunidad + '\'' +
+                ", provincia='" + provincia + '\'' +
+                '}';
+    }
 }
